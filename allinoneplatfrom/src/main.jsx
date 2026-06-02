30155a29490f2a84061fae624084a60860942665
sreenandhana <sreenandhan927@gmail.com>
2026-06-02 12:57:44 +0530

    Refactor routing to use HashRouter instead of BrowserRouter

--- allinoneplatfrom/src/main.jsx
+++ allinoneplatfrom/src/main.jsx
@@ -2,19 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import SearchContextShare from './context/SearchContextShare.jsx'
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENTID}>
-      <SearchContextShare>
-      <App />
-      </SearchContextShare>
+    <HashRouter>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENTID}>
+        <SearchContextShare>
+          <App />
+        </SearchContextShare>
       </GoogleOAuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
